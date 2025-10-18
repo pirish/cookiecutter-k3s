9bfd2a5866a4a4e27b693121a9c2393658f13d94
Patrick Irish <patscrap@gmail.com>
2025-10-17 23:52:05 -0300

    Add options for env secrets, rbac, multiple deployment types, backups, and certificates

--- {{ cookiecutter.project_slug }}/7b-backupinit.sh
+++ {{ cookiecutter.project_slug }}/7b-backupinit.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-restic -r s3:$(BUCKET_NAME):/$(CLUSTER_NAME)/{{ cookiecutter.project_slug }}-backup --verbose init
+restic -r s3:$(BUCKET_NAME)/{{ cookiecutter.cluster_name }}/{{ cookiecutter.project_slug }}-backup --verbose init
